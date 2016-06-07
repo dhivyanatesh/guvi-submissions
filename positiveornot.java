e64e3ed54b180f59983c745ff54b3fce813c5eeb
dhivyanatesh <dhivya.cs13@bitsathy.ac.in>
2016-06-07 11:49:08 +0500

    dummy creation

---
+++ positiveornot.java
@@ -0,0 +1,19 @@
+class viv
+{
+public static void main(String args[])
+{
+int a=10;
+if(a<0)
+{
+printf("negative");
+}
+elseif(a>=0)
+{
+printf("positive");
+}
+else
+{
+printf("invalid");
+}
+}
+}
