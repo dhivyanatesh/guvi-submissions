d15b643b156c5885f563d06b98c839128f9a4fe9
dhivyanatesh <dhivya.cs13@bitsathy.ac.in>
2016-06-07 14:48:01 +0500

    dummy creation

---
+++ positiverrnot.java
@@ -0,0 +1,20 @@
+class viv
+{
+public static void main(String args[])
+{
+ Scanner s = new Scanner(System.in);
+ int n = s.nextInt();
+if(a<0)
+{
+System.out.println("negative");
+}
+elseif(a>=0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("invalid");
+}
+}
+}
