fec22f9477ceb8a77c2a8a57460007daad08a2ab
dhivyanatesh <dhivya.cs13@bitsathy.ac.in>
2016-06-08 09:10:24 +0500

    dummyvariable

---
+++ primebwrange.java
@@ -0,0 +1,26 @@
+class primerange
+{
+public static void main()
+{
+int a,b,c,s,e;
+count=0;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter starting and ending limits");
+s=sc.nextInt();
+e=sc.nextInt();
+for(a=s;a<=e;a++)
+{
+c=0;
+for(b=2;b<i;b++)
+ {
+ if(b%a==0)
+ c++;
+ }
+ else
+ {
+ count++;
+ System.out.println("count is"+count);
+ }
+ }
+ }
+ }
